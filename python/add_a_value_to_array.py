56a2c3392e41c1f78ddf44ec1271f7d25ea06921
Sam-dev83 <amyrsalary1383@gmail.com>
2025-07-19 00:23:23 +0330

    Update and rename guess_number.py to add_a_value_to_array.py
    این کد به  تعداد ورودی ارایه درست میکند و مقادیر وارد شده را در ارایه ها ذخیره میکند

---
+++ python/add_a_value_to_array.py
@@ -0,0 +1,21 @@
+numbers = []
+i = 0
+print("برای پایان وارد کردن، '=' را وارد کنید.")
+
+while True:
+    user_input = input("یک عدد وارد کنید: ")
+    if user_input == "=":
+        break
+
+    try:
+        number = int(user_input)
+        # لیست را با افزودن عنصر جدید گسترش می‌دهیم
+        numbers =numbers + [0] # اضافه کردن فضای خالی (مثل append دستی)
+        numbers[i] = number  # مقداردهی دستی با اندیس
+        i += 1 # افزایش اندیس برای مقدار بعدی
+
+    except ValueError:
+        print("لطفاً فقط عدد وارد کنید.")
+
+print("مقادیر وارد شده:")
+print(numbers
